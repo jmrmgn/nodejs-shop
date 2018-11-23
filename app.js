5a544e3ebe35a2d1bb8ba07448e83e3ccf42d115
Jomar <jomar.magno026@gmail.com>
2018-11-24 00:07:16 +0800

    Implementing MVC architecture

--- app.js
+++ app.js
@@ -2,21 +2,17 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars'); // handlebars
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-// setting the template engine
-// app.engine('hbs', expressHbs({
-//    layoutsDir: 'views/layouts/',
-//    defaultLayout: 'main-layout',
-//    extname: 'hbs'
-// }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// Setting up EJS template engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
 // Getting the form within the body
 app.use(bodyParser.urlencoded({extended: false}));
@@ -25,19 +21,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 // Routes
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 // Displaying 404 page
-app.use((req, res, next) => {
-   // res.status(404).sendFile(
-   //    path.join(__dirname, 'views', '404.html')
-   // );
-   res.status(404).render('404', {
-      title: 'Page not Found',
-      path: ''
-   });
-});
+app.use(errorController.get404);
 
 // Server listens
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,33 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+// GET => /admin/add-product
+router.get('/add-product', productsController.getAddProduct);
 
-// /admin/add-product => GET
-router.get('/add-product', (req, res, next)=> {
-   // res.sendFile(
-   //    path.join(rootDir, 'views', 'add-product.html')
-   // );
-   res.render('add-product', {
-      title: 'Add product',
-      path: '/admin/add-product',
-      activeAddProduct: true
-   });
-});
+// POST => /admin/add-product => 
+router.post('/add-product', productsController.postAddProduct);
 
-// /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-   // console.log(req.body);
-   products.push({
-      title: req.body.title
-   })
-   res.redirect('/');
-   
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
