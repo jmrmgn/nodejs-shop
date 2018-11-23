0c460720e26ff2b76f9140a6c80b8a33e9d1d835
Jomar <jomar.magno026@gmail.com>
2018-11-23 21:40:01 +0800

    Pug template engine added

--- app.js
+++ app.js
@@ -5,20 +5,32 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+// setting the template engine
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+// Getting the form within the body
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public'))); //for static middleware(images,, .css, .js )
 
-app.use('/admin', adminRoutes);
+//for static middleware(images,, .css, .js )
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Routes
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 // Displaying 404 page
 app.use((req, res, next) => {
-   res.status(404).sendFile(
-      path.join(__dirname, 'views', '404.html')
-   );
+   // res.status(404).sendFile(
+   //    path.join(__dirname, 'views', '404.html')
+   // );
+   res.status(404).render('404', {
+      title: 'Page not Found'
+   });
 });
 
+// Server listens
 app.listen(3000);
