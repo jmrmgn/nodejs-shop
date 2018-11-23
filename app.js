cb6700078505f34d42177f542d3cb473f4ded2e1
Jomar <jomar.magno026@gmail.com>
2018-11-23 19:55:37 +0800

    Implementing Express.js

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-const http = require('http');
+const path = require('path');
 
-const server = http.createServer();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-server.listen(3000);
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public'))); //for static middleware(images,, .css, .js )
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// Displaying 404 page
+app.use((req, res, next) => {
+   res.status(404).sendFile(
+      path.join(__dirname, 'views', '404.html')
+   );
+});
+
+app.listen(3000);
